60c04979b6ccfbc68dbfe8c0626825b9b0bf276e
manik98 <manik.98.chakraborty@gmail.com>
2021-07-19 00:36:11 +0530

    manikchakrabortyweb

---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+function profileImageShow() {
+  var profileImage = document.getElementById('profile-image');
+  var profileText = document.getElementById('profile-text');
+  /* profileImage.style.width = "200px";
+  profileImage.style.height = "200px"; */
+  /* var x = window.matchMedia("(min-width: 1200px)");
+  if(x.matches){
+    profileImage.className = "profile-image-1400";
+  }else{
+    profileImage.className = "profile-image-1600";
+  } */
+  var screen1600 = window.matchMedia("(min-width: 1451px)");
+  var screen1400 = window.matchMedia("(min-width: 1250px)");
+  var screen700 = window.matchMedia("(min-width: 700px)");
+  if (screen1600.matches) {
+    profileImage.className = "profile-image-1600";
+    profileTextAnimation();
+  } else if (screen1400.matches) {
+    profileImage.className = "profile-image-1400";
+    profileTextAnimation();
+  } else if (screen700.matches) {
+    profileImage.className = "profile-image-700";
+    profileText.className = "profile-text-700";
+    profileTextAnimation();
+  }/* else{
+      profileImage.className = "profile-image-1600";
+    } */
+  /* profileImage.style.border = "10px solid white"; */
+  var text1 = document.getElementById('text1');
+  text1.style.width = "400px";
+}
+function profileTextAnimation() {
+  let i = 0;
+  setInterval(function () {
+    const textArr = ["Web Developer", "UI/UX Designer"];
+    var profileCaption = document.getElementById('profile-caption');
+    if (profileCaption.style.width == "400px") {
+      profileCaption.style.width = "0";
+    } else {
+      if (i === 2) i = 0;
+      profileCaption.style.width = "400px";
+      profileCaption.innerHTML = textArr[i];
+      i = i + 1;
+    }
+  }, 2000);
+}
+
+
+(function () {
+  'use strict';
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function (e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function () {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
+
+function progress(a, b) {
+  alert(a, b);
+}
