aff7f38ef8480072899bf01337c647eae4d11cec
manik98 <manik.98.chakraborty@gmail.com>
2021-07-19 03:46:19 +0530

    responsive change

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,7 @@ function profileImageShow() {
   var screen1600 = window.matchMedia("(min-width: 1451px)");
   var screen1400 = window.matchMedia("(min-width: 1250px)");
   var screen700 = window.matchMedia("(min-width: 700px)");
+  var phone = window.matchMedia("(max-width: 699px)");
   if (screen1600.matches) {
     profileImage.className = "profile-image-1600";
     profileTextAnimation();
@@ -22,9 +23,10 @@ function profileImageShow() {
     profileImage.className = "profile-image-700";
     profileText.className = "profile-text-700";
     profileTextAnimation();
-  }/* else{
-      profileImage.className = "profile-image-1600";
-    } */
+  }else if (phone.matches){
+      profileImage.className = "profile-image-phone";
+      profileTextAnimationPhone();
+  } 
   /* profileImage.style.border = "10px solid white"; */
   var text1 = document.getElementById('text1');
   text1.style.width = "400px";
@@ -44,6 +46,23 @@ function profileTextAnimation() {
     }
   }, 2000);
 }
+
+function profileTextAnimationPhone() {
+  let i = 0;
+  setInterval(function () {
+    const textArr = ["Web Developer", "UI/UX Designer"];
+    var profileCaption = document.getElementById('profile-caption');
+    if (profileCaption.style.width == "300px") {
+      profileCaption.style.width = "0";
+    } else {
+      if (i === 2) i = 0;
+      profileCaption.style.width = "300px";
+      profileCaption.innerHTML = textArr[i];
+      i = i + 1;
+    }
+  }, 2000);
+}
+
 
 
 (function () {
